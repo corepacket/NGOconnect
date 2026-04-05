ad6dd850ceb62dc0ee286c22d4a9f6c36876ed37
khushee23 <khuhs2519@gmail.com>
2026-04-05 18:59:00 +0530

    debugging

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {upload} from "../middlewares/multer.middleware.js"
-import { registerUser, loginUser, logoutUser, volunteerForEvent } from '../controllers/user.controller.js';
+import { registerUser, loginUser, logoutUser } from '../controllers/user.controller.js';
 
 const router = express.Router()
 
